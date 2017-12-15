d907d43c66ccdddb7a40fb09cab38658aa6e71f5
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-15 09:26:37 -0800

    Add tests for animal validations.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,23 @@
 class Animal < ApplicationRecord
+  validates_presence_of :name, :species, :sex, :birthday, :arrival_date
+  validate :birthday_valid
+  validate :arrival_date_valid
+
+  def birthday_valid
+    if birthday
+      unless (birthday < Date.today.next_day)
+        errors.add(:birthday, "can't be in the future")
+      end
+    end
+  end
+
+  def arrival_date_valid
+    if birthday && arrival_date
+      if (arrival_date < birthday)
+        errors.add(:arrival_date, "can't be before birthday")
+      elsif (arrival_date > Date.today.next_day)
+        errors.add(:arrival_date, "can't be in the future")
+      end
+    end
+  end
 end
---
+++ spec/models/animal_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe Animal do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :species }
+  it { should validate_presence_of :sex }
+  it { should validate_presence_of :birthday }
+  it { should validate_presence_of :arrival_date }
+
+  describe '#birthday_valid' do
+    it "should prevent save if birthday is not in the future" do
+      animal = FactoryBot.build(:animal, birthday: Date.today + 2.day, arrival_date: Date.today + 2.day)
+      expect(animal.save).to eq(false)
+    end
+
+    it "should allow save if birthday is today or earlier" do
+      animal = FactoryBot.build(:animal, birthday: Date.today, arrival_date: Date.today)
+      expect(animal.save).to eq(true)
+    end
+  end
+
+  describe '#arrival_date_valid' do
+    it "should prevent save if arrival date is before birthday" do
+      animal = FactoryBot.build(:animal, birthday: Date.today - 5.day, arrival_date: Date.today - 6.day)
+      expect(animal.save).to eq(false)
+    end
+
+    it "should prevent save if arrival date is in the future" do
+      animal = FactoryBot.build(:animal, arrival_date: Date.today + 7.day)
+      expect(animal.save).to eq(false)
+    end
+
+    it "should allow save if arrival date is between birthday and today, inclusive" do
+      animal = FactoryBot.build(:animal, arrival_date: Date.today, birthday: Date.today)
+      expect(animal.save).to eq(true)
+    end
+  end
+end
